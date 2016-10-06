64065cdfe907471c1ac1505a92b5698391ba6bca
krzysztof.gradzki <krzysztof.gradzki@icompass.pl>
2016-10-06 14:41:03 +0200

    add interception redirection to the 404 in the response error

--- src/app/core/core.httpinterceptor.js
+++ src/app/core/core.httpinterceptor.js
@@ -1,19 +1,32 @@
 (function () {
   module.exports = interceptHttp;
 
-  interceptHttp.$inject = ['$log', '$location'];
+  interceptHttp.$inject = ['$log', '$q', '$location', '$state'];
 
   /* eslint no-unused-vars: ["error", { "args": "none" }] */
-  function interceptHttp($log, $location) {
+  function interceptHttp($log, $q, $location, $state) {
     var interceptService = {
-      request: request
+      request: request,
+      response: response,
+      responseError: responseError
     };
 
     return interceptService;
 
     function request(config) {
-      $log.info("Url: %s", config.url);
       return config;
+    }
+
+    function response(response) {
+      return response;
+    }
+
+    function responseError(rejection) {
+      if (rejection.status === 404) {
+        $state.go('404');
+      }
+
+      return $q.reject(rejection);
     }
   }
 })();
